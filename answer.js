b155d3bcb300daaae45ab1095a94d376d248453a
ekachner <ekibam@gmail.com>
2019-11-21 19:06:19 -0700

    challenge 2 complete

--- answer.js
+++ answer.js
@@ -1,1 +1,9 @@
 // *** YOUR ANSWER BELOW ***
+
+function roundedDivision(x,y){
+    let divisor = x / y;
+    let remainder = x % y;
+    let subtractor = remainder / y;
+    return divisor - subtractor;
+}
+console.log(roundedDivision(9,4));
